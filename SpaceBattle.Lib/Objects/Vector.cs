fd92ab932b44ce39eae852a2216cad280f46157b
Fadeev <niugnep123lol@gmail.com>
2022-10-02 16:49:50 +0600

    Vector modified

--- SpaceBattle.Lib/Objects/Vector.cs
+++ SpaceBattle.Lib/Objects/Vector.cs
@@ -2,17 +2,29 @@ namespace SpaceBattle.Lib
 {
     public class Vector
     {
+        public int X { get; set; }
+        public int Y { get; set; }
+
         public Vector(int x, int y)
         {
             X = x;
             Y = y;
         }
 
-        public int X { get; set; }
-        public int Y { get; set; }
-
         public static Vector operator +(Vector v1, Vector v2)
-        => new Vector(v1.X + v2.X, v1.Y + v2.Y);
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y);
+        }
+
+        public static Vector operator -(Vector v1, Vector v2)
+        {
+            return new Vector(v1.X - v2.X, v1.Y - v2.Y);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y);
+        }
 
         public override bool Equals(object obj)
         {
@@ -22,13 +34,7 @@ namespace SpaceBattle.Lib
             {
                 return false;
             }
-
             return item.X == X && item.Y == Y;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(X, Y);
         }
     }
 }
