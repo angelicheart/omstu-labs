46732de214203e7be94fcfb662f9386dc036bce7
Arsenii Sotnikov <usersot1@gmail.com>
2022-12-19 21:22:34 +0600

    fix commit

--- SpaceBattle.Lib/Motion/EndMoveCommand.cs
+++ SpaceBattle.Lib/Motion/EndMoveCommand.cs
@@ -3,10 +3,12 @@ namespace SpaceBattle.Lib;
 public class EndMoveCommand : ICommand
 {
     private IMoveStopable ObjThatMove;
+
     public EndMoveCommand(IMoveStopable obj)
     {
         this.ObjThatMove = obj;
     }
+    
     public void Execute()
     {
         IoC.Resolve<ICommand>("Game.RemoveProperty", ObjThatMove.obj, "velocity", ObjThatMove.velocity).Execute();
--- SpaceBattle.Lib/Motion/IMoveStopable.cs
+++ SpaceBattle.Lib/Motion/IMoveStopable.cs
@@ -1,8 +1,8 @@
-using SpaceBattle.Lib;
+namespace SpaceBattle.Lib;
 
 public interface IMoveStopable
 {
     public IUObject obj { get; }
     public Vector velocity { get; }
+    public IQueue<ICommand> queue { get; }
 }
-
