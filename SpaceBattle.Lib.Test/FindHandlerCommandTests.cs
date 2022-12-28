f7e8245b1d2e365a546fc44ce946afd92da796a9
yukinon33 <niugnep123lol@gmail.com>
2022-12-28 07:32:35 +0600

    lab 5.1 start

---
+++ SpaceBattle.Lib.Test/FindHandlerCommandTests.cs
@@ -0,0 +1,70 @@
+namespace SpaceBattle.Lib.Test;
+
+public class FindExceptionCommandTests
+{
+    public FindExceptionCommandTests()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+    }
+
+    [Fact]
+    public void CommandNoThrowsExceptionTest()
+    {
+        var GameIsRunning = new Mock<IStrategy>();
+        GameIsRunning.Setup(crt => crt.Execute(It.IsAny<object[]>())).Returns((object) true);
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.IsRunning", (object[] args) => GameIsRunning.Object.Execute(args)).Execute();
+
+        var CommandStrategy = new Mock<IStrategy>();
+        CommandStrategy.Setup(s => s.Execute(It.IsAny<object[]>())).Throws(new Exception());
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Exception.FindHandlerWithTree", (object[] args) => CommandStrategy.Object.Execute()).Execute();
+        
+        var Command = new Mock<SpaceBattle.Lib.ICommand>();
+        Command.Setup(c => c.Execute());
+
+        var FindExceptionCommand = new FindExceptionCommand(Command.Object);
+        
+        FindExceptionCommand.Execute();
+    }
+
+    [Fact]
+    public void CommandThrowsExceptionTest()
+    {
+        var GameIsRunning = new Mock<IStrategy>();
+        GameIsRunning.Setup(crt => crt.Execute(It.IsAny<object[]>())).Returns((object) true);
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.IsRunning", (object[] args) => GameIsRunning.Object.Execute(args)).Execute();
+
+        var ExceptionHandlerCommand = new Mock<IStrategy>();
+
+        var CommandStrategy = new Mock<IStrategy>();
+        CommandStrategy.Setup(s => s.Execute(It.IsAny<object[]>())).Returns(ExceptionHandlerCommand.Object);
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Exception.FindHandlerWithTree", (object[] args) => CommandStrategy.Object.Execute()).Execute();
+        
+        var Command = new Mock<SpaceBattle.Lib.ICommand>();
+        Command.Setup(c => c.Execute()).Throws(new Exception());
+
+        var FindExceptionCommand = new FindExceptionCommand(Command.Object);
+        
+        FindExceptionCommand.Execute();
+    }
+
+    [Fact]
+    public void GameIsNotRunningTest()
+    {
+        var GameIsRunning = new Mock<IStrategy>();
+        GameIsRunning.Setup(crt => crt.Execute(It.IsAny<object[]>())).Returns((object) false);
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.IsRunning", (object[] args) => GameIsRunning.Object.Execute(args)).Execute();
+
+        var Command = new Mock<SpaceBattle.Lib.ICommand>();
+        Command.Setup(c => c.Execute());
+
+        var FindExceptionCommand = new FindExceptionCommand(Command.Object);
+        
+        Assert.ThrowsAny<Exception>(() => FindExceptionCommand.Execute());
+    }
+}
---
+++ SpaceBattle.Lib/Commands/FindExceptionHandlerCommand.cs
@@ -0,0 +1,28 @@
+namespace SpaceBattle.Lib;
+
+public class FindExceptionCommand : ICommand
+{
+    private readonly ICommand Command;
+    
+    public FindExceptionCommand(ICommand command)
+    {
+        this.Command = command;
+    }
+
+    public void Execute()
+    {
+        if (IoC.Resolve<bool>("Game.IsRunning"))
+        {
+            try
+            {
+                this.Command.Execute();
+            }
+
+            catch (Exception Exception)
+            {
+                IoC.Resolve<IStrategy>("Game.Exception.FindHandlerWithTree", Command, Exception).Execute();
+            }
+        }
+        else throw new Exception("GAME ISNT RUNNING");
+    }
+}
