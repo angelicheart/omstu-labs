30a8aff2ea71252050ff188923e437efba8f25a4
yukinon33 <niugnep123lol@gmail.com>
2022-12-29 17:13:43 +0600

    lab5 with conditions done

---
+++ SpaceBattle.Lib/Strategies/FindExcHanlderWithTreeStrategy.cs
@@ -0,0 +1,19 @@
+namespace SpaceBattle.Lib;
+
+public class FindHandlerWithTreeStrategy : IStrategy
+{
+    public object Execute(params object[] args)
+    {
+        ICommand Command = (ICommand) args[0];
+        Exception exception = (Exception) args[1];
+
+        Int32 HashOfCommand = Command.GetType().GetHashCode();
+        Int32 HashOfException = exception.GetType().GetHashCode();
+
+        var ExceptionHandlerTree = IoC.Resolve<IReadOnlyDictionary<Int32, IReadOnlyDictionary<Int32, IStrategy>>>("Game.Exception.GetTree");
+
+        var SubTree = IoC.Resolve<IReadOnlyDictionary<Int32, IStrategy>>("Game.Exception.GetSubTree");
+
+        return ExceptionHandlerTree[HashOfCommand].TryGetValue(HashOfException, out var value) ? value : SubTree.TryGetValue(HashOfException, out var val) ? val : null;
+    }
+}
