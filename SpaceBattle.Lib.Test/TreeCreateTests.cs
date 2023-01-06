190375bcde1700098379ecf2e8b7888e8e6c86fa
Arsenii Sotnikov <usersot1@gmail.com>
2023-01-06 19:53:35 +0600

    fix

--- SpaceBattle.Lib.Test/TreeCreateTests.cs
+++ SpaceBattle.Lib.Test/TreeCreateTests.cs
@@ -5,12 +5,9 @@ public class TreeCreateTest
     public TreeCreateTest()
     {
         new InitScopeBasedIoCImplementationCommand().Execute();
-
-        new InitScopeBasedIoCImplementationCommand().Execute();
         IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
 
         var TreeStrategy = new TreeCreateStrategy();
-
         IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Tree.Build", (object[] args) => (TreeStrategy.Execute(args))).Execute();
     }
 
