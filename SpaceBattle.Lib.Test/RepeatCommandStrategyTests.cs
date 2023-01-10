1c7d1f0e510d5326e0705d77f08d23d7ec6e9414
Arsenii Sotnikov <usersot1@gmail.com>
2023-01-11 03:16:06 +0600

    Tests add

---
+++ SpaceBattle.Lib.Test/RepeatCommandStrategyTests.cs
@@ -0,0 +1,38 @@
+namespace SpaceBattle.Lib.Test;
+
+public class RepeatCommandStrategyTests
+{
+    public RepeatCommandStrategyTests()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+
+        var Command = new Mock<SpaceBattle.Lib.ICommand>();
+        Command.Setup(c => c.Execute());
+
+        var CommandStrategy = new Mock<IStrategy>();
+        CommandStrategy.Setup(s => s.Execute(It.IsAny<object[]>())).Returns(Command.Object);
+
+        var InjectCommand = new InjectCommand(Command.Object);
+        var InjectStrategy = new Mock<IStrategy>();
+        InjectStrategy.Setup(s => s.Execute(It.IsAny<object[]>())).Returns(InjectCommand);
+
+        var RepeatCommand = new RepeatCommand(Command.Object);
+        var RepeatStrategy = new Mock<IStrategy>();
+        RepeatStrategy.Setup(s => s.Execute(It.IsAny<object[]>())).Returns(RepeatCommand);
+
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Commands.MacroCommand", (object[] args) => CommandStrategy.Object.Execute(args)).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Commands.InjectCommand", (object[] args) => InjectStrategy.Object.Execute(args)).Execute();
+        IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "Game.Commands.RepeatCommand", (object[] args) => RepeatStrategy.Object.Execute(args)).Execute();
+    }
+
+    [Fact]
+    public void RepeatCommandStrategyPositive()
+    {
+        var TestObj = new Mock<IUObject>();
+        var Operation = "Operation.Name";
+        var Command = new RepeatCommandStrategy();
+
+        Command.Execute(Operation, TestObj.Object);
+    }
+}
