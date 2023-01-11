baeec60d8e91f0d5c88c0bd08a1b2c0931316970
Arsenii Sotnikov <usersot1@gmail.com>
2023-01-11 12:12:53 +0600

    fix

--- SpaceBattle.Lib/Strategies/RepeatCommandStrategy.cs
+++ SpaceBattle.Lib/Strategies/RepeatCommandStrategy.cs
@@ -7,7 +7,7 @@ public class RepeatCommandStrategy : IStrategy
         string operation = (string) args[0];
         IUObject obj = (IUObject) args[1];
 
-        var mcd = IoC.Resolve<ICommand>("Game.Commands.MacroCommand");
+        var mcd = IoC.Resolve<ICommand>("Game.Commands.MacroCommand", operation, obj);
         var inject = IoC.Resolve<ICommand>("Game.Commands.InjectCommand", mcd);
         var repeat = IoC.Resolve<ICommand>("Game.Commands.RepeatCommand", inject);
 
