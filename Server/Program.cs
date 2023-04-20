af9126131ecbf2057b04d47b2f0a1fcf55792dac
Arsenii Sotnikov <usersot1@gmail.com>
2023-04-20 21:23:45 +0600

    new tests

---
+++ Server/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SpaceBattle.Lib;
+
+class Program
+{
+    static void Main(string[] args)
+    {   
+        int n_threads = int.Parse(args[0]);
+        var bla = new StartServer(n_threads);
+    }
+}
