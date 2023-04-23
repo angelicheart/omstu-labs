f8ad41ff9018c43c361aa152bbc2b603e1c6cedb
Arsenii Sotnikov <usersot1@gmail.com>
2023-04-23 23:33:54 +0600

    pls help me

--- Server/Program.cs
+++ Server/Program.cs
@@ -5,6 +5,7 @@ class Program
     static void Main(string[] args)
     {   
         int n_threads = int.Parse(args[0]);
-        var bla = new StartServer(n_threads);
+        var server = new StartServer(n_threads);
+        server.Execute();
     }
 }
