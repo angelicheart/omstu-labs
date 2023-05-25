79337662cd1fe58cc57591be22626a3aba163785
yukinon33 <niugnep123lol@gmail.com>
2023-05-25 09:24:32 +0600

    lab #8 gamecommand finished, codestyle fixed

--- SpaceBattle.Lib.Test/GameCommandTests.cs
+++ SpaceBattle.Lib.Test/GameCommandTests.cs
@@ -5,7 +5,6 @@ public class GameCommandTests
     public GameCommandTests()
     {
         new InitScopeBasedIoCImplementationCommand().Execute();
-        // IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
     }
 
     [Fact]
