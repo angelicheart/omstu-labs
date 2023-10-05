f187e80c70c259964ab8ed4fd693b56c98695934
Арсений Сотников <usersot1@gmail.com>
2023-10-05 21:46:25 +0600

    IMessage

---
+++ SpaceBattle.Lib/Objects/IMessage.cs
@@ -0,0 +1,10 @@
+using System.Globalization;
+
+namespace SpaceBattle.Lib;
+
+public interface IMessage
+{
+    public string CmdType { get; } 
+    public int GameID { get; }
+    public int ItemID { get; }
+}
